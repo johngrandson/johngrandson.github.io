67dc2ca41465c13c614b5ffcddc282b9bfebd8a9
João Netto <joaonettopb@hotmail.com>
2025-10-12 03:52:32 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import "./assets/aos.css";
 import "./assets/animated.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import '../node_modules/et-line/style.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import './assets/style.scss';
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "../node_modules/et-line/style.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "./assets/style.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
